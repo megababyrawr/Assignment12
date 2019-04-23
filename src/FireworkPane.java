d7b307999c09583971770119a4f67791da007776
megababyrawr <babyrawr321@gmail.com>
2019-04-23 00:45:56 -0700

    Initial commit

---
+++ src/FireworkPane.java
@@ -0,0 +1,43 @@
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
+public class FireworkPane {
+    private double centerX;
+    private double centerY;
+    private double radius = 25.0;
+    private Color color;
+    private int beamNum;
+    private double step = 2.0;
+    private double angleSize;
+    private Timeline timelin1;
+
+    public FireworkPane(Color initColor, int initInt) {
+
+
+    }
+
+    public void resume() {
+
+    }
+
+    public void suspend() {
+
+    }
+
+    public void changeColor(Color input) {
+
+    }
+
+    public void setBeamNumber(int a) {
+
+    }
+
+    public void setRate(int a) {
+
+    }
+
+}
